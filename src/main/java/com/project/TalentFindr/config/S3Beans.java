760f79d83a6702ef98d59aa9cf2b641f84deef74
Aryan Sankar <145859525+ironfist-15@users.noreply.github.com>
2025-10-09 02:00:43 +0530

    27:add value specifically

--- src/main/java/com/project/TalentFindr/config/S3Beans.java
+++ src/main/java/com/project/TalentFindr/config/S3Beans.java
@@ -1,19 +1,32 @@
 package com.project.TalentFindr.config;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+ // New Import
+
 
 @Configuration
 public class S3Beans {
-        @Bean
-        public S3Client s3Client() {
-            return S3Client.builder().region(Region.US_EAST_1).build();
-        }
 
-        @Bean
-        public String bucketName() {
-            return "talentfindr-bucket";
-        }
+    @Value("talentfindr-bucket")
+    private String bucketName;
+
+    @Value("us-east-1")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                 .build();
+    }
+
+    @Bean
+    public String bucketName() {
+        return bucketName;
+    }
 }
